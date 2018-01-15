0f8c038023e341ce5af3b633c899dc2ae2a48233
Aaron L. Zeng <me@bcc32.com>
2018-01-15 03:55:26 -0500

    Version 0.03

--- lib/App/WatchLater.pm
+++ lib/App/WatchLater.pm
@@ -19,11 +19,11 @@ App::WatchLater - Manage your YouTube Watch Later videos
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
--- lib/App/WatchLater/Browser.pm
+++ lib/App/WatchLater/Browser.pm
@@ -10,11 +10,11 @@ App::WatchLater::Browser - Open URLs in the User's Browser
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
